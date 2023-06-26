b0ce58e4075d16724c536bd8dfcee543a5776a65
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2023-06-26 09:25:01 +0530

    Add. Added File Component with CSS file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import FileUploadComponent from './Components/FileUploadComponent/FileUploadComponent';
+// import SongGalleryComponent from './Components/SongGalleryComponent/SongGalleryComponent';
 
 function App() {
   return (
-    <div className="App">
-     React App
-    </div>
+    <Router>
+      <div className='container'>
+        <h1>Music Player App</h1>
+
+        <nav className='nav-menu'>
+          <Link to='/'>Home</Link>
+          <Link to='/admin/upload'>Add Songs</Link>
+        </nav>
+        <Routes>
+          {/* <Route exact path='/' element={<SongGalleryComponent />} /> */}
+          <Route path='/admin/upload' element={<FileUploadComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
